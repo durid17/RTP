05e8da48219fd4f5bafaaad72c8e9f64544aea3e
durid17 <48883403+durid17@users.noreply.github.com>
2019-11-10 19:45:39 +0400

    Add files via upload

--- Sender.py
+++ Sender.py
@@ -13,10 +13,70 @@ class Sender(BasicSender.BasicSender):
         self.sackMode = sackMode
         self.debug = debug
 
+    def handshake(self):
+        for i in range (0 , 20):
+            self.send(self.make_packet('syn' , 0 , ''))
+            ans = self.receive(1)
+            if ans is None: continue
+            return True
+        return False
+
     # Main sending loop.
     def start(self):
-      # add things here
-      pass
+        sz = 300
+        b = self.handshake()
+        # print(b)
+        if not b: return
+        data = self.infile.read()
+        chunks = []
+        chunks.append(b"das")
+        for i in range(0 , len(data) , sz):
+            chunks.append(data[i :min(i + sz , len(data))])
+        # print(chunks)
+        to_send = {}
+        for i in range(1 , min(8 , len(chunks))):
+            to_send[i] = chunks[i]
+        index = 1
+        while True:
+            if len(to_send) == 0: break
+            for ind in to_send.keys():
+                self.send(self.make_packet('dat' , ind , to_send[ind]))
+            answers = []
+            while True:
+                ans = self.receive(0.5)
+                if ans is None: break
+                if Checksum.validate_checksum(ans):
+                    answers.append(ans)
+
+            c = 0
+            for elem in answers:
+                msg_type, seqno, data, _ = self.split_packet(elem)
+                if(msg_type == 'ack' and seqno == index):c+=1
+            if(c >= 4):
+                to_send.clear()
+                to_send[index] = chunks[index]
+                continue
+            
+            for elem in answers:
+                msg_type, seqno_str, data, _ = self.split_packet(elem)
+                if(msg_type == 'ack' ): 
+                    seqno = int(seqno_str)
+                    index = max(index , seqno)   
+                if(msg_type == 'sack') :
+                    pieces = elem.split('|')        
+                    index = max(index , int(pieces[1].split(';')[0]))   
+                    ls = pieces[1].split(';')[1].split(',')
+                    for ind in ls:
+                        if(ind == ''): continue
+                        if(int(ind) in to_send.keys()) :del to_send[int(ind)]                
+                   
+            to_send.clear()
+            for i in range(index , min(index + 7 , len(chunks))):
+                to_send[i] = chunks[i]
+        
+        self.send(self.make_packet('fin' , index , ''))
+        pass
+        
         
 '''
 This will be run if you run this script from the command line. You should not
@@ -63,3 +123,34 @@ if __name__ == "__main__":
         s.start()
     except (KeyboardInterrupt, SystemExit):
         exit()
+
+
+
+
+# python TestHarness.py -s Sender.py -r Receiver.py
+# python TestHarness.py -s Sender.py -r Receiver.py
+# python TestHarness.py -s Sender.py -r Receiver.py
+# python TestHarness.py -s Sender.py -r Receiver.py
+# python TestHarness.py -s Sender.py -r Receiver.py
+# python TestHarness.py -s Sender.py -r Receiver.py
+# python TestHarness.py -s Sender.py -r Receiver.py
+# python TestHarness.py -s Sender.py -r Receiver.py
+# python TestHarness.py -s Sender.py -r Receiver.py
+# python TestHarness.py -s Sender.py -r Receiver.py
+# python TestHarness.py -s Sender.py -r Receiver.py
+# python TestHarness.py -s Sender.py -r Receiver.py
+# python TestHarness.py -s Sender.py -r Receiver.py
+# python TestHarness.py -s Sender.py -r Receiver.py
+# python TestHarness.py -s Sender.py -r Receiver.py
+# python TestHarness.py -s Sender.py -r Receiver.py
+# python TestHarness.py -s Sender.py -r Receiver.py
+# python TestHarness.py -s Sender.py -r Receiver.py
+# python TestHarness.py -s Sender.py -r Receiver.py
+# python TestHarness.py -s Sender.py -r Receiver.py
+# python TestHarness.py -s Sender.py -r Receiver.py
+# python TestHarness.py -s Sender.py -r Receiver.py
+# python TestHarness.py -s Sender.py -r Receiver.py
+# python TestHarness.py -s Sender.py -r Receiver.py
+# python TestHarness.py -s Sender.py -r Receiver.py
+# python TestHarness.py -s Sender.py -r Receiver.py
+# python TestHarness.py -s Sender.py -r Receiver.py
